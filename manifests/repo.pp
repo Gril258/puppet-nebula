1a2f7fae621a3878e006a36858e4cf80b0153c39
Martin Krutina <gril51043@gmail.com>
2023-07-18 19:50:51 +0200

    add support for debian 12

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -8,6 +8,18 @@ class nebula::repo (
   case $::operatingsystem {
     'debian': {
       case $::lsbdistcodename {
+        'bookworm': {
+          apt::source { 'opennebula':
+            location => "https://downloads.opennebula.io/repo/${version}/Debian/12",
+            release  => 'stable',
+            repos    => 'opennebula',
+            pin      => '600',
+            key      => {
+              id     => '0B2D385C7C9304B11A0367B905A05927906DC27C',
+              source => 'https://downloads.opennebula.io/repo/repo2.key',
+            },
+          }
+        }
         'bullseye': {
           apt::source { 'opennebula':
             location => "https://downloads.opennebula.io/repo/${version}/Debian/11",
